5dfa390ba1144687c233df5070748e51eea32ac0
UdaykiranLues <uday@scienstechnologies.com>
2026-04-09 11:53:51 +0530

    feat: override onPause in MainActivity to allow continuous background audio execution

--- android/app/src/main/java/com/usmusic/app/MainActivity.java
+++ android/app/src/main/java/com/usmusic/app/MainActivity.java
@@ -2,4 +2,13 @@ package com.usmusic.app;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onPause() {
+        super.onPause();
+        // Prevent WebView from pausing when app is in the background
+        if (this.bridge != null && this.bridge.getWebView() != null) {
+            this.bridge.getWebView().onResume();
+        }
+    }
+}
